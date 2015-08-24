6bf83e671dab2ecd3b4a4ee47fbaf99cf3dfdf6a
helloaihao <543160349@qq.com>
2015-08-24 23:50:03 +0800

    update dir

--- JavaScriptDOM编程艺术/10/js/positionMessage.js
+++
@@ -1,9 +0,0 @@
-function positionMessage() {
-	var elem = document.getElementById("message");
-	elem.style.position = "absolute";
-	elem.style.left = "50px";
-	elem.style.top = "100px";
-	moveElement("message", 200, 200, 10);
-}
-
-addLoadEvent(positionMessage);
