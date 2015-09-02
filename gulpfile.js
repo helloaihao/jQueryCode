971d14e8a625ba4a65f34e422bdc5e2e13647f36
helloaihao <543160349@qq.com>
2015-09-03 01:19:25 +0800

    add bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 gulp.task('default', function() {
 	gulp.run('jquery');
+	gulp.run('bootstrap');
 });
 
 gulp.task('jquery', function() {
 	gulp.src(['./bower_components/jquery/dist/jquery.min.js'])
 	.pipe(rename('jquery.js'))
+	.pipe(gulp.dest('jQueryCode/'));
+});
+
+gulp.task('bootstrap', function() {
+	gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.min.css',
+	 './bower_components/bootstrap/dist/js/bootstrap.min.js'])
 	.pipe(gulp.dest('jQueryCode/'));
 });
